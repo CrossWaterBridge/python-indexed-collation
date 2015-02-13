36d6949fe4fc9cba6918d42ab1645deb60a4a7db
Hilton Campbell <hiltonc@ldsmobileapps.com>
2015-02-12 20:23:50 -0700

    Include install requirements.

--- setup.py
+++ setup.py
@@ -7,4 +7,7 @@ setup(
     license='MIT',
     description='Python package that provide localized indexed collations.',
     long_description=open('README.md').read(),
+    install_requires=[
+        'PyICU==1.8',
+    ],
 )
